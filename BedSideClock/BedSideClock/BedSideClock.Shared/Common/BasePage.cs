b11bc475373aaea4403e812245a9523a9fdcf168
David Underhill <d.underhill@techsmith.com>
2014-04-23 13:57:09 -0400

    Updated code to use a BasePage that contained Nav Helper
    Added a base class for the pages that contains the NavigationHelper
    class baked in.

--- BedSideClock/BedSideClock/BedSideClock.Shared/Common/BasePage.cs
+++ BedSideClock/BedSideClock/BedSideClock.Shared/Common/BasePage.cs
@@ -1,10 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
 
 namespace BedSideClock.Common
 {
-    class BasePage
+  //this was done based on work if found in this blog post and helps
+  // reduce the code in the code behind pages so that logic there is specific to the 
+  // applicatoin and not house cleaning type stuff.
+  //http://marcominerva.wordpress.com/2013/10/10/a-base-page-class-for-windows-8-1-store-apps-with-c-and-xaml/
+  public class BasePage : Page
+  {
+    private readonly NavigationHelper navigationHelper;
+    private readonly ObservableDictionary defaultViewModel = new ObservableDictionary();
+
+
+    public BasePage()
     {
+      this.navigationHelper = new NavigationHelper(this);
+      this.navigationHelper.LoadState += navigationHelper_LoadState;
+      this.navigationHelper.SaveState += navigationHelper_SaveState;
     }
+
+    public ObservableDictionary DefaultViewModel
+    {
+      get { return this.defaultViewModel; }
+    }
+
+    public NavigationHelper NavigationHelper
+    {
+      get { return this.navigationHelper; }
+    }
+
+    private void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
+    {
+      LoadState(e);
+    }
+
+    private void navigationHelper_SaveState(object sender, SaveStateEventArgs e)
+    {
+      SaveState(e);
+    }
+
+    protected virtual void LoadState(LoadStateEventArgs e) { }
+    protected virtual void SaveState(SaveStateEventArgs e) { }
+
+    #region NavigationHelper registration
+
+    protected override void OnNavigatedTo(NavigationEventArgs e)
+    {
+      navigationHelper.OnNavigatedTo(e);
+    }
+
+    protected override void OnNavigatedFrom(NavigationEventArgs e)
+    {
+      navigationHelper.OnNavigatedFrom(e);
+    }
+
+    #endregion
+  }
 }
--- BedSideClock/BedSideClock/BedSideClock.WindowsPhone/View/StartupPage.xaml.cs
+++ BedSideClock/BedSideClock/BedSideClock.WindowsPhone/View/StartupPage.xaml.cs
@@ -5,16 +5,16 @@ using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
-using BedSideClock.PhoneSessionManager;
+using BedSideClock.Common;
 using BedSideClock.PhoneSessionManager.Interfaces;
-using BedSideClock.WinPhoneSessionManagers;
+
 
 namespace BedSideClock.View
 {
   /// <summary>
   ///   An empty page that can be used on its own or navigated to within a Frame.
   /// </summary>
-  public sealed partial class StartupPage : Page, IFileOpenPickerContinuable
+  public sealed partial class StartupPage : BasePage, IFileOpenPickerContinuable
   {
     public static StartupPage Current;
 
@@ -23,16 +23,7 @@ namespace BedSideClock.View
       InitializeComponent();
     }
 
-    /// <summary>
-    ///   Invoked when this page is about to be displayed in a Frame.
-    /// </summary>
-    /// <param name="e">
-    ///   Event data that describes how this page was reached.
-    ///   This parameter is typically used to configure the page.
-    /// </param>
-    protected override void OnNavigatedTo(NavigationEventArgs e)
-    {
-    }
+
 
     private void AppBarButton_Click(object sender, RoutedEventArgs e)
     {
