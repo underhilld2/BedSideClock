3e2b1ac32aaa56126a056a179dea9f03df745b0b
David Underhill <d.underhill@techsmith.com>
2014-07-12 18:18:05 -0400

    Refactored location to own namespace

---
+++ BedSideClock/SharedLogicLibrary/RandomLocation/RandomLocationGenerator.cs
@@ -0,0 +1,67 @@
+﻿using SharedLogicLibrary.Logic;
+
+namespace SharedLogicLibrary.RandomLocation
+{
+   public class RandomLocationGenerator : PropertyChangeLogic
+   {
+
+
+      private int _maxWidth;
+      private int _maxHeight;
+      private int _currentHorizontalLocation;
+      private int _currentVerticalLocation;
+
+      public int MaxWidth
+      {
+
+         get
+         {
+            return _maxWidth;
+
+         }
+         set
+         {
+            _maxWidth = value;
+            NotifyPropertyChanged( () => MaxWidth );
+         }
+      }
+      public int MaxHeight
+      {
+
+         get
+         {
+            return _maxHeight;
+
+         }
+         set
+         {
+            _maxHeight = value;
+            NotifyPropertyChanged( () => MaxHeight );
+         }
+      }
+
+      public int CurrentHorizontalLocation
+      {
+         get { return _currentHorizontalLocation; }
+         set
+         {
+            _currentHorizontalLocation = value;
+            NotifyPropertyChanged( () => CurrentHorizontalLocation );
+         }
+      }
+      public int CurrentVerticalLocation
+      {
+         get
+         {
+            return _currentVerticalLocation;
+         }
+         set
+         {
+            _currentVerticalLocation = value;
+            NotifyPropertyChanged( () => CurrentVerticalLocation );
+         }
+      }
+
+      
+   }
+}
---
+++ BedSideClock/SharedLogicLibrary/RandomLocation/ScreenLocation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLogicLibrary.RandomLocation
+{
+   public class ScreenLocation
+   {
+   }
+}
